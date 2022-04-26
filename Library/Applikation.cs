793ffcb9f20a5d4488b53a25a8172eb4a635de85
Даниил <kourin.daniil@gmail.com>
2022-04-26 09:27:44 +0300

    Добавьте файлы проекта.

---
+++ Library/Applikation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Library
+{
+    public class Applikation
+    {
+        private Application application;
+        private List<TaskList> taskLists;
+
+
+    }
+}
---
+++ Library/TaskList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class TaskList
+    {
+        private string _name;
+        private List<TaskList> _tasks;
+
+
+    }
+}
